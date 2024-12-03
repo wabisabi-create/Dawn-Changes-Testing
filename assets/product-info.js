ca5a9c5876eb0914af651683dc5e465a86edc57a
Sudhanshu Sam <thomsoiitr15@gmail.com>
2024-12-03 20:39:01 +0530

    testing random 11

--- assets/product-info.js
+++ assets/product-info.js
@@ -73,10 +73,9 @@ if (!customElements.get('product-info')) {
           this.renderProductInfo({
             requestUrl: this.buildRequestUrlWithParams(productUrl, selectedOptionValues, shouldFetchFullPage),
             targetId: target.id,
-            // callback: shouldSwapProduct
-            //   ? this.handleSwapProduct(productUrl, shouldFetchFullPage)
-            //   : this.handleUpdateProductInfo(productUrl),
-            callback: this.handleSwapProduct(productUrl, shouldFetchFullPage)
+            callback: shouldSwapProduct
+              ? this.handleSwapProduct(productUrl, shouldFetchFullPage)
+              : this.handleUpdateProductInfo(productUrl),
           });
         }
   
@@ -88,59 +87,6 @@ if (!customElements.get('product-info')) {
   
         handleSwapProduct(productUrl, updateFullPage) {
           return (html) => {
-            // const variant = this.getSelectedVariant(html);
-
-            const variantValues = variant.options
-            const mediaGallery = document.querySelector(`[id^="MediaGallery-${this.dataset.section}"]`)
-            if (mediaGallery.hasAttribute("media-grouping-enabled")) {
-              mediaGallery.querySelectorAll('[data-media-group]').forEach(el => el.classList.add('hide-media'))
-              variantValues.forEach(value => { mediaGallery.querySelectorAll(`[data-media-group="${value}"]`).forEach(el => el.classList.remove('hide-media')) })
-              mediaGallery.querySelectorAll('slider-component').forEach(slider => { slider.initPages() })
-            }
-  
-            this.pickupAvailability?.update(variant);
-            this.updateOptionValues(html);
-            this.updateURL(productUrl, variant?.id);
-            this.updateVariantInputs(variant?.id);
-  
-            if (!variant) {
-              this.setUnavailable();
-              return;
-            }
-  
-            this.updateMedia(html, variant?.featured_media?.id);
-  
-            const updateSourceFromDestination = (id, shouldHide = (source) => false) => {
-              const source = html.getElementById(`${id}-${this.sectionId}`);
-              const destination = this.querySelector(`#${id}-${this.dataset.section}`);
-              if (source && destination) {
-                destination.innerHTML = source.innerHTML;
-                destination.classList.toggle('hidden', shouldHide(source));
-              }
-            };
-  
-            updateSourceFromDestination('price');
-            updateSourceFromDestination('Sku', ({ classList }) => classList.contains('hidden'));
-            updateSourceFromDestination('Inventory', ({ innerText }) => innerText === '');
-            updateSourceFromDestination('Volume');
-            updateSourceFromDestination('Price-Per-Item', ({ classList }) => classList.contains('hidden'));
-  
-            this.updateQuantityRules(this.sectionId, html);
-            this.querySelector(`#Quantity-Rules-${this.dataset.section}`)?.classList.remove('hidden');
-            this.querySelector(`#Volume-Note-${this.dataset.section}`)?.classList.remove('hidden');
-  
-            this.productForm?.toggleSubmitButton(
-              html.getElementById(`ProductSubmitButton-${this.sectionId}`)?.hasAttribute('disabled') ?? true,
-              window.variantStrings.soldOut
-            );
-  
-            publish(PUB_SUB_EVENTS.variantChange, {
-              data: {
-                sectionId: this.sectionId,
-                html,
-                variant,
-              },
-            });               
             this.productModal?.remove();
   
             const selector = updateFullPage ? "product-info[id^='MainProduct']" : 'product-info';
@@ -163,7 +109,8 @@ if (!customElements.get('product-info')) {
                 this.preProcessHtmlCallbacks,
                 this.postProcessHtmlCallbacks
               );
-            }             
+            }    
+            this.updateMedia(html, variant?.featured_media?.id);         
           };    
         }
   
